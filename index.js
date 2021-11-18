62f2321cfbdb02a3778a3c7735729252c0ead762
FC <falcu2345@gmail.com>
2021-11-18 12:08:37 -0500

    Configure nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('¡Hola mundo de Express!');
+    res.send('¡Hola mundo de Express JS!');
 });
 
 app.listen(5000, function() {
