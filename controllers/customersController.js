8eeeee2f7e8d7819d0ce81ed2c724b2d92e312fd
FC <falcu2345@gmail.com>
2021-11-18 14:07:55 -0500

    add customer

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,5 +1,19 @@
 const Customers = require('../models/Customers');
 
+// agregar cliente
+exports.add = async(req, res) => {
+    const customer = new Customers(req.body);
+
+    try {
+        await customer.save();
+        res.json({ message: 'Nuevo cliente agregado' });
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+        next();
+    }
+};
+
 // Primera acción: list
 exports.list = async(req, res) => {
     try {
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const customersController = require('../controllers/customersController');
 
 module.exports = function() {
+    // post: /customers
+    router.post('/customers', customersController.add);
     // get: /customers
     router.get('/customers', customersController.list);
 
